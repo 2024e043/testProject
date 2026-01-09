f056b5401f3d9c9ccd890811bfa070e65e8830cc
2024e043 <2024e043@hokuriku-u.ac.jp>
2026-01-09 14:29:04 +0900

    Add allowed hosts for deployment

--- devProject/settings.py
+++ devProject/settings.py
@@ -129,3 +129,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+ALLOWED_HOSTS = [
+    "testproject-m5c7.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
